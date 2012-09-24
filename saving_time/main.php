edd33b194e7f787e21fd648e3bb76980fd710320
nirasan <niranirao@gmail.com>
2012-09-24 10:03:39 +0900

    for http://codegolf.com/saving-time

---
+++ saving_time/main.php
@@ -0,0 +1,23 @@
+<?php
+    # for http://codegolf.com/saving-time
+    list($h, $m) = explode(':', trim(fgets(STDIN)));
+    $h = $h % 12;
+    $m = (int)($m / 5);
+    for ($i = 0; $i < 12; $i++) {
+        $c[$i] = 2*$i-$h-$m ? $i-$h ? $i-$m ? o : m : h : x;
+    }
+    $format = <<<EOL
+%9s
+%5s%8s
+
+%2s%14s
+
+%s%16s
+
+%2s%14s
+
+%5s%8s
+%9s
+EOL;
+    printf($format, $c[0], $c[11], $c[1], $c[10], $c[2], $c[9], $c[3], $c[8], $c[4], $c[7], $c[5], $c[6]);
+?>
